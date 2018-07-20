b4ebd0cb308780fa07eb1b3be027c9561ee486d9
momoiroshikibu <momoiroshikibu@gmail.com>
2018-07-20 22:57:09 +0900

    fixed typo

--- flaskr/db.py
+++ flaskr/db.py
@@ -6,11 +6,11 @@ from flask.cli import with_appcontext
 
 def get_db():
     if 'db' not in g:
-        g.db = sqlite.connect(
+        g.db = sqlite3.connect(
             current_app.config["DATABASE"],
             detect_types=sqlite3.PARSE_DECLTYPES
         )
-        d.db.row_factory = sqlite3.Row
+        g.db.row_factory = sqlite3.Row
     return g.db
 
 def close_db(e=None):
